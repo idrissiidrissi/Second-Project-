76234fa09dccd87fea6d133a05cfd1e16df66a93
mohamed tazi <idrissi2929@gmail.com>
2022-06-12 21:01:59 -0400

    decrement.increment function added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let shop = document.getElementById("shop");
 
 let shopItemsData = [{
     id:"hello",
-    name: "pomegranate",
+    name: "Organic Pomegranate",
     price: 4.99,
     desc: "Fresh Organic NON GMO local grew Pomegranate.",
     img: "images/pomegranate.jpg"
@@ -10,24 +10,24 @@ let shopItemsData = [{
 },
 {
     id:"helloo",
-    name: "pear",
+    name: "ORGANIC Pears",
     price: 3.59,
-    desc: "Fresh Organic NON GMO local grew Pomegranate.",
+    desc: "Fresh Organic NON GMO local grew pears.",
     img: "images/pear.jpg"
 },
 {
     id:"hellooo",
     name: "peaches",
     price: 1.99,
-    desc: "Fresh Organic NON GMO local grew Pomegranate.",
-    img: "images/pomegranate.jpg"
+    desc: "Fresh Organic NON GMO local grew peaches.",
+    img: "images/peaches.jpg"
 },
 {
     id:"hellooo",
-    name: "grapes",
+    name: "ORGANIC Grapes",
     price: 2.99,
-    desc: "Fresh Organic NON GMO local grew Pomegranate.",
-    img: "images/pomegranate.jpg"
+    desc: "Fresh Organic NON GMO local grew grapes.",
+    img: "images/grapes.jpg"
 },
 {
     id:"helloooo",
@@ -38,22 +38,27 @@ let shopItemsData = [{
 },
 ];
 
+let basket = [{
+    id:"hello",
+    item: 1
+}]
+
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData
         .map((x) => {
-            let { id, name, price, img } = x;
+            let { id, name, price, img, desc} = x;
        return `
        <div class="item">
        <img width="220" src=${img} alt="">
        <div class="details">
        <h3>${name}</h3>
-       <p>Fresh Organic NON GMO local grew Pomegranate.</p>
+       <p>${desc}</p>
        <div class="price-quantity">
        <h2>$ ${price} per-Lip</h2>
        <div class="buttons">
-         <i class="bi bi-dash-circle-fill"></i>
-         <div class="quantity">0</div>
-           <i class="bi bi-plus-circle-fill"></i>
+         <i onclick="decrement(${id})" class="bi bi-dash-circle-fill"></i>
+         <div id=${id} class="quantity">0</div>
+           <i onclick="increment(${id})" class="bi bi-plus-circle-fill"></i>
            </div>
            </div>
        </div>
@@ -65,3 +70,65 @@ let generateShop = () => {
 };
 
 generateShop();
+
+/**
+ * ! used to increase the selected product item quantity by 1
+ */
+
+ let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    if (search === undefined) {
+      basket.push({
+        id: selectedItem.id,
+        item: 1,
+      });
+    } else {
+      search.item += 1;
+    }
+  
+    console.log(basket);
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+  };
+
+  /**
+ * ! used to decrease the selected product item quantity by 1
+ */
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+  
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+      search.item -= 1;
+    }
+  
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    console.log(basket);
+    localStorage.setItem("data", JSON.stringify(basket));
+  };
+
+  /**
+ * ! To update the digits of picked items on each item card
+ */
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+  };
+
+  /**
+ * ! To calculate total amount of selected Items
+ */
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  };
+  
+  calculation();
