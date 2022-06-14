2e96fad8fdb23313b8673a3dbfc3fb223172d50d
idrissiidrissi <97397543+idrissiidrissi@users.noreply.github.com>
2022-06-14 13:18:45 -0400

    modified

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 let shop = document.getElementById("shop");
 
-let shopItemsData = [{
-    id:"hello",
+let shopItemsData = [
+{
+    id:"a",
     name: "Organic Pomegranate",
     price: 4.99,
     desc: "Fresh Organic NON GMO local grew Pomegranate.",
     img: "images/pomegranate.jpg"
-
 },
 {
-    id:"helloo",
+    id:"b",
     name: "ORGANIC Pears",
     price: 3.59,
     desc: "Fresh Organic NON GMO local grew pears.",
     img: "images/pear.jpg"
 },
 {
-    id:"hellooo",
+    id:"c",
     name: "peaches",
     price: 1.99,
     desc: "Fresh Organic NON GMO local grew peaches.",
     img: "images/peaches.jpg"
 },
 {
-    id:"hellooo",
+    id:"d",
     name: "ORGANIC Grapes",
     price: 2.99,
     desc: "Fresh Organic NON GMO local grew grapes.",
     img: "images/grapes.jpg"
 },
 {
-    id:"helloooo",
+    id:"f",
     name: "pomegranate",
     price: 3.99,
     desc: "Fresh Organic NON GMO local grew Pomegranate.",
@@ -38,17 +38,15 @@ let shopItemsData = [{
 },
 ];
 
-let basket = [{
-    id:"hello",
-    item: 1
-}]
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData
         .map((x) => {
-            let { id, name, price, img, desc} = x;
+            let { id, name, price, desc, img } = x;
+            let search = basket.find((x) => x.id === id) || [];
        return `
-       <div class="item">
+       <div id=product-id-${id} class="item">
        <img width="220" src=${img} alt="">
        <div class="details">
        <h3>${name}</h3>
@@ -57,7 +55,9 @@ let generateShop = () => {
        <h2>$ ${price} per-Lip</h2>
        <div class="buttons">
          <i onclick="decrement(${id})" class="bi bi-dash-circle-fill"></i>
-         <div id=${id} class="quantity">0</div>
+         <div id=${id} class="quantity">
+         ${search.item === undefined? 0 : search.item}
+         </div>
            <i onclick="increment(${id})" class="bi bi-plus-circle-fill"></i>
            </div>
            </div>
@@ -66,6 +66,7 @@ let generateShop = () => {
  `;
     })
     .join(""));
+    
     
 };
 
@@ -76,59 +77,55 @@ generateShop();
  */
 
  let increment = (id) => {
-    let selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem.id);
-    if (search === undefined) {
-      basket.push({
-        id: selectedItem.id,
-        item: 1,
-      });
-    } else {
-      search.item += 1;
-    }
-  
-    console.log(basket);
-    update(selectedItem.id);
-    localStorage.setItem("data", JSON.stringify(basket));
-  };
+    let selctedItem = id;
+    let search = basket.find((x) => x.id === selctedItem.id);
+    if(search === undefined) {
+        basket.push({
+            id: selctedItem.id,
+            item: 1,
+    });
+} else {
+    search.item += 1;
+}
+localStorage.setItem("data", JSON.stringify(basket));
+    //console.log(basket);
+    update(selctedItem.id);
+ };
 
-  /**
+/**
  * ! used to decrease the selected product item quantity by 1
  */
 
-let decrement = (id) => {
-    let selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem.id);
-  
+ let decrement = (id) => {
+    let selctedItem = id;
+    let search = basket.find((x) => x.id === selctedItem.id);
     if (search === undefined) return;
-    else if (search.item === 0) return;
-    else {
-      search.item -= 1;
-    }
-  
-    update(selectedItem.id);
-    basket = basket.filter((x) => x.item !== 0);
-    console.log(basket);
+    else if(search.item === 0) return;
+    
+ else {
+    search.item -= 1;
+}
+update(selctedItem.id);
+basket = basket.filter((x) => x.item !== 0);
+    //console.log(basket);
+    
     localStorage.setItem("data", JSON.stringify(basket));
-  };
-
-  /**
- * ! To update the digits of picked items on each item card
- */
-
-let update = (id) => {
+ };
+ 
+ let update = (id) => {
     let search = basket.find((x) => x.id === id);
+    //console.log(search.item);
     document.getElementById(id).innerHTML = search.item;
     calculation();
-  };
+ };
 
-  /**
- * ! To calculate total amount of selected Items
- */
-
-let calculation = () => {
+ let calculation = () => {
     let cartIcon = document.getElementById("cartAmount");
-    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
-  };
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=>x+y, 0)
+    
+ };
+
+ calculation()
+
+
   
-  calculation();
