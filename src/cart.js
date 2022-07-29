021cbfe3756c591707d642ef2c4f58ebad1bbf38
idrissiidrissi <97397543+idrissiidrissi@users.noreply.github.com>
2022-07-29 11:58:53 -0400

    last

--- src/cart.js
+++ src/cart.js
@@ -201,7 +201,6 @@ let TotalAmount = () => {
   </div>
   <div id="overlay"></div>
   </div>
-
   <button onclick="clearCart()" class="removeAll">Clear Cart</button>
 
     `);
@@ -298,43 +297,42 @@ window.clearCart = clearCart
   *! Checkout buttons
   */
 
-const openOrderButtons = document.querySelectorAll('[data-order-target]')
-const closeOrderButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openOrderButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const order = document.querySelector(button.dataset.orderTarget)
-    openOrder(order)
+  const openOrderButtons = document.querySelectorAll('[data-order-target]')
+  const closeOrderButtons = document.querySelectorAll('[data-close-button]')
+  const overlay = document.getElementById('overlay')
+  
+  openOrderButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const order = document.querySelector(button.dataset.orderTarget)
+      openOrder(order)
+    })
   })
-})
-
-overlay.addEventListener('click', function (){
-    const orders = document.querySelectorAll('.order.active');
-    orders.forEach(order => {
-      closeOrder(order);
-    });
+  
+  overlay.addEventListener('click', function (){
+      const orders = document.querySelectorAll('.order.active');
+      orders.forEach(order => {
+        closeOrder(order);
+      });
+    })
+  
+  closeOrderButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const order = button.closest('.order')
+      closeOrder(order)
+    })
   })
-
-closeOrderButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const order = button.closest('.order')
-    closeOrder(order)
-  })
-})
-
-function openOrder(order) {
-  if (order == null) return
-  order.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeOrder(order) {
-  if (order == null) return
-  order.classList.remove('active')
-  overlay.classList.remove('active')
-}
-
+  
+  function openOrder(order) {
+    if (order == null) return
+    order.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeOrder(order) {
+    if (order == null) return
+    order.classList.remove('active')
+    overlay.classList.remove('active')
+  }
  
 
  
